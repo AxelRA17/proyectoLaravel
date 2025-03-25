e938ed156601ab6c5f547402404183338d18b7ab
AxelRA17 <ronquilloandradeaxel@gmail.com>
2025-03-24 23:51:53 -0600

    proyecto blade

--- resources/views/plantilla.blade.php
+++ resources/views/plantilla.blade.php
@@ -5,11 +5,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     
-    <!-- Bootstrap -->
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-    <!-- Fuente pixelada y CSS personalizado -->
     <link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('css/main.css') }}">
     
@@ -18,7 +17,6 @@
 
 <body>
 
-    <!-- Encabezado -->
     <header class="text-center py-4">
         <h1 class="minecraft-title">Minecraft Wiki</h1>
     </header>
@@ -34,7 +32,6 @@
         <p>&copy; Todos los derechos reservados para Axel Ronquillo :p</p>
     </footer>
 
-    <!-- Scripts -->
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"
         integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous">
     </script>
